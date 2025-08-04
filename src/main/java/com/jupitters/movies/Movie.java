bbb80344faddfe4adbebbfabf7f0d018cef8f2bc
jupitters <sampaiobale@gmail.com>
2025-08-04 17:14:46 -0300

    movies

---
+++ src/main/java/com/jupitters/movies/Movie.java
@@ -0,0 +1,18 @@
+package com.jupitters.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+public class Movie {
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
