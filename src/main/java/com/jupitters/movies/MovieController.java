e6f9a10378b85abe3f7b3d264b0ec09f795a2cf1
jupitters <sampaiobale@gmail.com>
2025-08-04 17:26:32 -0300

    controller

--- src/main/java/com/jupitters/movies/MovieController.java
+++ src/main/java/com/jupitters/movies/MovieController.java
@@ -1,4 +1,14 @@
 package com.jupitters.movies;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+    @GetMapping
+    public String allMovies() {
+        return "All movies";
+    }
 }
