42a673d94fb8a35ce51195444fa42f58dbe4e8dd
jupitters <sampaiobale@gmail.com>
2025-08-05 14:28:28 -0300

    movie service

--- src/main/java/com/jupitters/movies/MovieService.java
+++ src/main/java/com/jupitters/movies/MovieService.java
@@ -2,7 +2,13 @@ package com.jupitters.movies;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MovieService {
+    private MovieRepository movieRepository;
 
+    public List<Movie> allMovies() {
+
+    }
 }
