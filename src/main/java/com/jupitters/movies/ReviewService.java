c2f969dcb03542195fec0f09922e904b919e5964
jupitters <sampaiobale@gmail.com>
2025-08-06 17:49:14 -0300

    review

--- src/main/java/com/jupitters/movies/ReviewService.java
+++ src/main/java/com/jupitters/movies/ReviewService.java
@@ -1,6 +1,9 @@
 package com.jupitters.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +13,17 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-        reviewRepository.insert(review);
+    public Review createReview(String reviewBody, String imdbId){
+       Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }
